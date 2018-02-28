f9ab902a71bebca62641d4aa523aeb02aa46d117
Harindi <supunsara.harindi@gmail.com>
2018-02-28 08:25:54 +0530

    Jasper Reports.

--- Dashboard/src/main/java/org/arpicoinsurance/groupit/dashboard/controller/JasperReportController.java
+++ Dashboard/src/main/java/org/arpicoinsurance/groupit/dashboard/controller/JasperReportController.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
