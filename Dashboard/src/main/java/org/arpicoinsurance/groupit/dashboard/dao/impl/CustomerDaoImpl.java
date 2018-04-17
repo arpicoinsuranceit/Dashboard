eccd40f6677bb7177a9043dfdfcee53cffd4bfb9
Anjana Thrishakya <anjanathrishakya@gmail.com>
2018-04-17 11:22:01 +0530

    Fic Dob Bug

--- Dashboard/src/main/java/org/arpicoinsurance/groupit/dashboard/dao/impl/CustomerDaoImpl.java
+++ Dashboard/src/main/java/org/arpicoinsurance/groupit/dashboard/dao/impl/CustomerDaoImpl.java
@@ -1,5 +1,6 @@
 package org.arpicoinsurance.groupit.dashboard.dao.impl;
 
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
 
 import org.arpicoinsurance.groupit.dashboard.dao.CustomerDao;
@@ -15,16 +16,17 @@ public class CustomerDaoImpl implements CustomerDao {
 
 	@Override
 	public String save(HashMap<String, Object> details) throws Exception {
-		
+
 		jdbcTemplate.update(
 				"insert into marksys.incustomers (sbucod, cscode, ppdnam, ppddob, ppdnag, ppdsex, \r\n"
 						+ "ppdcst, ppdtel, sponam, sagnxt, ntitle, spodob, numchl, ppdnic, sponic) \r\n"
-						+ "value ('450',?,?,?,?,?,?,?,?,?,?,?,?,?,?);",
+						+ "value ('450',?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
 				new Object[] { (String) details.get("cscode"), (String) details.get("ppdnam"),
-						(String) details.get("ppddob"), Integer.parseInt(details.get("ppdnag").toString()),
-						(String) details.get("ppdsex"), (String) details.get("ppdcst"), (String) details.get("ppdtel"),
-						(String) details.get("sponam"), Integer.parseInt(details.get("sagnxt").toString()),
-						(String) details.get("ntitle"), (String) details.get("spodob"),
+						new SimpleDateFormat("dd-MM-yyyy").parse((String) details.get("ppddob")),
+						Integer.parseInt(details.get("ppdnag").toString()), (String) details.get("ppdsex"),
+						(String) details.get("ppdcst"), (String) details.get("ppdtel"), (String) details.get("sponam"),
+						Integer.parseInt(details.get("sagnxt").toString()), (String) details.get("ntitle"),
+						details.get("spodob") != null ? new SimpleDateFormat("dd-MM-yyyy").parse((String) details.get("spodob")) : null,
 						Integer.parseInt(details.get("numchl").toString()), (String) details.get("ppdnic"),
 						(String) details.get("sponic")
 
