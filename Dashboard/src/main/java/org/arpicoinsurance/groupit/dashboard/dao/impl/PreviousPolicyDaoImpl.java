f33fbb0c8501567f3e86b709c00e269e2d4d9117
Anjana Thrishakya <anjanathrishakya@gmail.com>
2018-05-22 15:30:35 +0530

    Try Catch PreviousPolicyDao

--- Dashboard/src/main/java/org/arpicoinsurance/groupit/dashboard/dao/impl/PreviousPolicyDaoImpl.java
+++ Dashboard/src/main/java/org/arpicoinsurance/groupit/dashboard/dao/impl/PreviousPolicyDaoImpl.java
@@ -18,13 +18,15 @@ public class PreviousPolicyDaoImpl implements PreviousPolicyDao {
 	public HashMap<String, Object> getsumAtRisk(String nic) throws Exception {
 		// System.out.println(nic);
 		HashMap<String, Object> details = null;
-		details = jdbcTemplate.queryForObject(
-				"select sum(sumrkm) as sumAtRisk, max(cscode) as custCode from inproposals a \r\n"
-						+ "where a.sbucod='450' and a.pprsta <> 'INAC' and pprsta in ('PLISU','LAMD') \r\n"
-						+ "and (a.polnum is not null or polnum <> '') and TRIM(a.ppdnic)= ? \r\n"
-						+ "and TIMESTAMPDIFF(YEAR,icpdat,sysdate()) <=  2 group by a.ppdnic",
-				new Object[] { nic }, new PreviousPolicyRowMapper());
-
+		try {
+			details = jdbcTemplate.queryForObject(
+					"select sum(sumrkm) as sumAtRisk, max(cscode) as custCode from inproposals a \r\n"
+							+ "where a.sbucod='450' and a.pprsta <> 'INAC' and pprsta in ('PLISU','LAMD') \r\n"
+							+ "and (a.polnum is not null or polnum <> '') and TRIM(a.ppdnic)= ? \r\n"
+							+ "and TIMESTAMPDIFF(YEAR,icpdat,sysdate()) <=  2 group by a.ppdnic",
+					new Object[] { nic }, new PreviousPolicyRowMapper());
+		} catch (Exception e) {
+		}
 		return details != null ? details : new HashMap<>();
 	}
 
